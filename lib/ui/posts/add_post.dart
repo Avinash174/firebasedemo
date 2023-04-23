565ce7b295e8f07dee9deaaf5b52f64d39219179
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2023-04-23 23:48:22 +0530

    New Changes Done

--- lib/ui/posts/add_post.dart
+++ lib/ui/posts/add_post.dart
@@ -1,3 +1,6 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebasedemo/utils/utils.dart';
+import 'package:firebasedemo/widgets/round_button.dart';
 import 'package:flutter/material.dart';
 
 class AddPost extends StatefulWidget {
@@ -8,11 +11,55 @@ class AddPost extends StatefulWidget {
 }
 
 class _AddPostState extends State<AddPost> {
+  bool loading = false;
+  final databaseRef = FirebaseDatabase.instance.ref('Post');
+  final postController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Add Post'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            TextFormField(
+              maxLines: 5,
+              controller: postController,
+              decoration: InputDecoration(
+                hintText: 'Enter is in your mind?',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            RoundButton(
+                title: 'Add Post',
+                loading: loading,
+                onTap: () {
+                  setState(() {
+                    loading=true;
+                  });
+                  databaseRef.child(DateTime.now().millisecondsSinceEpoch.toString()).set({
+                    'title':postController.text.toString(),
+                    'id':DateTime.now().millisecondsSinceEpoch.toString()
+                  }).then((value) {
+                    Utils().toastMsg('Post Added');
+                  }).onError((error, stackTrace) {
+                    Utils().toastMsg(error.toString());
+                    setState(() {
+                      loading=false;
+                    });
+                  });
+                }),
+          ],
+        ),
       ),
     );
   }

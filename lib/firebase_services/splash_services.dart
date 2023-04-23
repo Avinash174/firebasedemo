2dd11eba6f1446cbf4e488910f49d99150d02b12
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2023-04-23 16:14:21 +0530

    New Changes Done

--- lib/firebase_services/splash_services.dart
+++ lib/firebase_services/splash_services.dart
@@ -1,12 +1,26 @@
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebasedemo/ui/auth/login_Screen.dart';
+import 'package:firebasedemo/ui/posts/post_screen.dart';
 import 'package:flutter/material.dart';
 
 class SplashServices {
+  final _auth=FirebaseAuth.instance;
+  final user =_auth.currentUser;
   void isLogin(BuildContext context) {
-    Timer(
-        const Duration(seconds: 3),
-        () => Navigator.push(
-            context, MaterialPageRoute(builder: (_) =>const LogInScreen())));
+
+    if(user !=null){
+      Timer(
+          const Duration(seconds: 3),
+              () => Navigator.push(
+              context, MaterialPageRoute(builder: (_) =>const PostScreen())));
+    }
+    else{
+      Timer(
+          const Duration(seconds: 3),
+              () => Navigator.push(
+              context, MaterialPageRoute(builder: (_) =>const LogInScreen())));
+    }
+
   }
 }

45f0f8b9a15450294d911244a3bc6b29023436b6
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2023-04-24 18:41:45 +0530

    New Changes Done

--- lib/ui/posts/post_screen.dart
+++ lib/ui/posts/post_screen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:firebasedemo/ui/posts/add_post.dart';
+import 'package:firebasedemo/utils/utils.dart';
 import 'package:flutter/material.dart';
 
 class PostScreen extends StatefulWidget {
@@ -15,6 +16,7 @@ class _PostScreenState extends State<PostScreen> {
   final auth = FirebaseAuth.instance;
   final ref = FirebaseDatabase.instance.ref('Post');
   final searchController = TextEditingController();
+  final editController = TextEditingController();
 
   @override
   void initState() {
@@ -83,18 +85,25 @@ class _PostScreenState extends State<PostScreen> {
                   final title = Text(snapshot.child('id').value.toString());
                   if (searchController.text.isEmpty) {
                     return ListTile(
-                      title: Text(snapshot.child('id').value.toString(),),
-                      subtitle: Text(snapshot.child('title').value.toString(),),
+                      title: Text(
+                        snapshot.child('title').value.toString(),
+                      ),
+                      subtitle: Text(
+                        snapshot.child('id').value.toString(),
+                      ),
                       trailing: PopupMenuButton(
-                        icon: Icon(Icons.more_vert),
+                        icon: Icon(
+                          Icons.more_vert,
+                        ),
                         itemBuilder: (BuildContext context) => [
                           PopupMenuItem(
-                            onTap: (){
-                              Navigator.pop(context);
-                              showMyDilog();
-                            },
                             value: 1,
                             child: ListTile(
+                              onTap: () {
+                                Navigator.pop(context);
+                                showMyDilog(title.toString(),
+                                    snapshot.child('id').value.toString());
+                              },
                               leading: Icon(Icons.edit),
                               title: Text('Edit'),
                             ),
@@ -102,21 +111,25 @@ class _PostScreenState extends State<PostScreen> {
                           PopupMenuItem(
                             value: 1,
                             child: ListTile(
+                              onTap: () {
+                                Navigator.pop(context);
+                                showMyDilog(title.toString(),
+                                    snapshot.child('id').value.toString());
+                              },
                               leading: Icon(Icons.delete),
                               title: Text('Delete'),
                             ),
                           ),
                         ],
                       ),
-
                     );
                   } else if (title
                       .toString()
                       .toLowerCase()
                       .contains(searchController.toString().toLowerCase())) {
                     return ListTile(
-                      title: Text(snapshot.child('id').value.toString()),
-                      subtitle: Text(snapshot.child('title').value.toString()),
+                      title: Text(snapshot.child('title').value.toString()),
+                      subtitle: Text(snapshot.child('id').value.toString()),
                     );
                   } else {
                     return Container();
@@ -133,19 +146,43 @@ class _PostScreenState extends State<PostScreen> {
       ),
     );
   }
-  Future<void>showMyDilog(){
-    return showDialog(context: context, builder: (BuildContext context){
-      return AlertDialog(
-        title: Text('Update'),
-        content: Container(
-          child: TextField(
 
-          ),
-        ),
-        actions: [],
+  Future<void> showMyDilog(String title, String id) async {
+    editController.text = title;
 
-      );
 
-    });
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Update'),
+            content: Container(
+              child: TextField(
+                controller: editController,
+                decoration: InputDecoration(),
+              ),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Cancel')),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    ref
+                        .child(id)
+                        .update({'title': editController.text.toString()}).then(
+                            (value) {
+                      Utils().toastMsg('Post Updated');
+                    }).onError((error, stackTrace) {
+                      Utils().toastMsg(error.toString());
+                    });
+                  },
+                  child: Text('Update')),
+            ],
+          );
+        });
   }
 }
